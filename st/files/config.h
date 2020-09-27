5a9ad4abe921764565aafb00b3a500e7ef79b1cb
Eudald Gubert i Roldan <hola@eudald.gr>
2020-09-27 17:05:41 +0000

    st: update config

--- st/files/config.h
+++ st/files/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 30;
 
 /*
